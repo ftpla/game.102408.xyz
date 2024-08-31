e3733da7e20821351c57a15ce407a034681f5c7a
ftpla <166898937+ftpla@users.noreply.github.com>
2024-08-31 17:36:04 +0800

    Add files via upload

---
+++ js/personalize_color.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+	
+	if (window.localStorage.getItem("color")==null) {
+		window.localStorage.setItem("color","hsla(144,54%,30%,100%)");
+	}
+	
+	document.getElementById("h-top").style.backgroundColor=window.localStorage.getItem("color");
+}
+
+window.addEventListener("storage", function (e) {
+	if(e.key == "color" && e.newValue) {
+		document.getElementById("h-top").style.backgroundColor=window.localStorage.getItem("color");
+	}
+});
---
+++ js/theme.js
@@ -0,0 +1,27 @@
+//获取系统主题
+window.onload=function(){
+	const mediaQueryListDark = window.matchMedia('(prefers-color-scheme: dark)');
+	if (mediaQueryListDark.matches) {
+	  // 系统当前是暗色(dark)主题
+	}
+	
+	const mediaQueryListLight = window.matchMedia('(prefers-color-scheme: light)');
+	if (mediaQueryListLight.matches) {
+	  // 系统当前是亮色(light)主题
+	}
+}
+
+window.onload=function(){
+	
+	if (window.localStorage.getItem("theme")==null) {
+		window.localStorage.setItem("theme",0);
+	}
+	
+	document.getElementsByTagName("body").style.backgroundColor=window.localStorage.getItem("color");
+}
+
+window.addEventListener("storage", function (e) {
+	if(e.key == "color" && e.newValue) {
+		document.getElementById("h-top").style.backgroundColor=window.localStorage.getItem("color");
+	}
+});
